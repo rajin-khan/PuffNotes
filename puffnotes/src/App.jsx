9510a4a51078e7e64e3ef1d82caabe453fcd1a95
Rajin Khan <rajin.khan2001@gmail.com>
2025-04-30 18:32:55 +0600

    Begin proje ct

---
+++ puffnotes/src/App.jsx
@@ -0,0 +1,7 @@
+export default function App() {
+    return (
+      <div className="min-h-screen bg-white flex items-center justify-center">
+        <h1 className="text-3xl font-serif text-accent">hello, puffnotes ✨</h1>
+      </div>
+    )
+  }
