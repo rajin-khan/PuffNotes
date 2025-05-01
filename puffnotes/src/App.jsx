052746ceca29641437023f69e0833331acb39751
Rajin Khan <rajin.khan2001@gmail.com>
2025-05-02 01:45:18 +0600

    Optimize video

--- puffnotes/src/App.jsx
+++ puffnotes/src/App.jsx
@@ -185,6 +185,7 @@ export default function App() {
         playsInline
         className="fixed top-0 left-0 w-full h-full object-cover z-[10] pointer-events-none"
       >
+        <source src="/background.webm" type="video/webm" />
         <source src="/background.mp4" type="video/mp4" />
         Your browser does not support the video tag.
       </video>
